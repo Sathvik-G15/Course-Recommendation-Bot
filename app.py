cffcdc9d24cad52fc6e98dc2e1fe7a9644c11ea1
Sathvik <sathvikguntupalli2005@gmail.com>
2025-06-27 21:23:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -94,6 +94,6 @@ def recommend():
         "query": raw_query
     })
 
-if __name__ == "__main__":
-    from waitress import serve
-    serve(app, host="0.0.0.0", port=5000)
+# if __name__ == "__main__":
+#     from waitress import serve
+#     serve(app, host="0.0.0.0", port=5000)
