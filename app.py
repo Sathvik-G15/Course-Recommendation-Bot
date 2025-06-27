025c5ee7b81a6431344a84099ebb3d5627bcd8fc
Sathvik <sathvikguntupalli2005@gmail.com>
2025-06-27 21:35:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -93,7 +93,3 @@ def recommend():
         "recommendations": recommendations or [],
         "query": raw_query
     })
-
-# if __name__ == "__main__":
-#     from waitress import serve
-#     serve(app, host="0.0.0.0", port=5000)
