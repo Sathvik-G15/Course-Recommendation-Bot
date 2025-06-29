9144339b14475b30e8ce6c58d1d8e12236919bd4
Sathvik <sathvikguntupalli2005@gmail.com>
2025-06-29 21:18:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -94,5 +94,11 @@ def recommend():
         "recommendations": recommendations or [],
         "query": raw_query
     })
+if __name__ == "__main__":
+    from waitress import serve
+    import os
+    port = int(os.environ.get("PORT", 5000))  # Render sets this
+    serve(app, host="0.0.0.0", port=port)
+
 
 
